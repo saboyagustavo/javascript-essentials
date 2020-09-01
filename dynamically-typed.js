419372588d3edacf14d41586562e82c680efe547
Gustavo Saboya <saboya.gustavo@gmail.com>
2020-09-01 16:02:55 -0300

    Deeping into dynamically-typed examples.

--- dynamically-typed.js
+++ dynamically-typed.js
@@ -1,4 +1,11 @@
 //no classes, parser just assigned the type of variable at runtime;
 var number = 1;
+
+console.log(number==1) //expected output: true
+console.log(number==="1") //expected output: false
+
 number = "regular text";
 console.log(number); //expected output: "regular text"
+
+number = +'1';
+console.log(typeof number); //expected output: number
