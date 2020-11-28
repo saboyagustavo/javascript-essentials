5f847331f69907c0be505f5e69828080c3e724e5
Gustavo Saboya <saboya.gustavo@gmail.com>
2020-11-28 17:19:02 -0300

    Exemplifying the concept of boolean primitive type.

---
+++ boolean.js
@@ -0,0 +1,8 @@
+// boolean primitives are logical data type that bears only the value of true or false
+let bool = true;
+console.log(bool); //-> true
+
+bool = !bool;
+console.log(bool); //-> false
+
+// useful to decide which sections of code to execute
